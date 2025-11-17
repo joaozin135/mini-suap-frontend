fa5d704a449b6fe1b8c4f5fff350f8959707d380
João Junior <junior.joao1305@gmail.com>
2025-11-17 19:58:37 -0300

    att components

--- assets/components/Button/index.tsx
+++ assets/components/Button/index.tsx
@@ -1,19 +1,19 @@
 import { type LucideIcon } from "lucide-react"
-import { Link } from "react-router-dom";
+import { Link, type LinkProps } from "react-router-dom";
 
-interface ButtonMassaProps {
+interface ButtonMassaProps extends LinkProps {
     icon: LucideIcon;
     text: string;
     to: string;
-    className?: string;
 }
 
-export function ButtonMassa({ icon: Icon, text, to, className}: ButtonMassaProps){
+export function ButtonMassa({ icon: Icon, text, to, className, ...props}: ButtonMassaProps){
    return(
         <Link
             to={to}
             id={`button-${text.toLowerCase().replace(/\s/g, '-')}`}
             className={`flex items-center w-full gap-2 p-2 rounded hover:bg-white/10 transition-colors ${className}`} 
+            {...props}
         >     
             <div className="justify-start items-center flex p-4 gap-3">
                 <Icon size={22} />
--- assets/components/SideBar/index.tsx
+++ assets/components/SideBar/index.tsx
@@ -1,5 +1,6 @@
 import { LucideHouse, LucidePhone, LucideSquareChartGantt } from "lucide-react"
 import { ButtonMassa } from "../Button"
+import { useLocation } from "react-router-dom";
 
 
 interface SideBarProps {
@@ -7,6 +8,7 @@ interface SideBarProps {
 }
 
 export function SideBar({ text }: SideBarProps){
+    const {pathname} = useLocation()
     return(
         <aside className="bg-[#1C385B] size-full min-h-dvh flex flex-col gap-8 max-w-72 text-white p-4">
                 <h1 className="font-semibold text-3xl text-center">{text}</h1>
@@ -16,16 +18,22 @@ export function SideBar({ text }: SideBarProps){
                             icon={LucideHouse}
                             text="Dashboard"
                             to="/"
+                            data-active={pathname === "/"}
+                            className="data-[active=true]:bg-white/10"
                         />
                         <ButtonMassa 
                             icon={LucidePhone}
                             text="Chamados"
                             to="/chamados"
+                            data-active={pathname === "/chamados"}
+                            className="data-[active=true]:bg-white/10"
                         />
                         <ButtonMassa 
                             icon={LucideSquareChartGantt}
                             text="Relatórios"
                             to="/relatorios"
+                            data-active={pathname === "/relatorios"}
+                            className="data-[active=true]:bg-white/10"
                         />
                     </div>
                 </article>
