cd4d77c142b084c40164648a90f9772d21e526b9
João Junior <junior.joao1305@gmail.com>
2025-11-18 09:11:54 -0300

    refactored components dashboard screen

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,19 +5,17 @@ import { CardTop } from "../../../assets/components/card";
 export function Dashboard (){
     return(
         <main className="flex items-start size-full bg-[#F6F9FC]">
-            <SideBar 
-                text="Mini Suap"
-            />
+            <SideBar text="Mini Suap"/>
             <aside className="size-full p-4">
                 <nav className="flex justify-end items-center">
                     <div id="card" className=" flex flex-col rounded-2xl w-fit">
-                                <div id="card-header" className="p-3 flex items-center gap-3">
-                                    <div id="card-icon" className="p-2 rounded-full bg-[#D9D9D9]">
-                                        <LucideUserRound size={22} />  
-                                    </div>
-                                    <h1 className="text-lg font-medium">user</h1>
-                                </div>
+                        <div id="card-header" className="p-3 flex items-center gap-3">
+                            <div id="card-icon" className="p-2 rounded-full bg-[#D9D9D9]">
+                                <LucideUserRound size={22} />  
                             </div>
+                            <h1 className="text-lg font-medium">user</h1>
+                        </div>
+                    </div>
                 </nav>
                 <section className="space-y-12">
                     <article className="space-y-8">
