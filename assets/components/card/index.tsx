29950e170e66dbfd24c40783120a52ad01e1a298
João Junior <junior.joao1305@gmail.com>
2025-11-17 21:47:58 -0300

    add props

--- assets/components/card/index.tsx
+++ assets/components/card/index.tsx
@@ -1,15 +1,16 @@
 import type { LucideIcon } from "lucide-react";
+import type { DetailedHTMLProps } from "react";
 
 
-interface cardProps {
+interface cardProps extends DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     icon: LucideIcon;
     text: string;
     value: string;
 }
 
-export function CardTop({ icon: Icon, text, value}: cardProps){
+export function CardTop({ icon: Icon, text, value, className, ...props}: cardProps){
     return(
-            <div id="card" className="bg-[#DCE9F5] flex flex-col rounded-2xl w-lg h-32">
+            <div id="card" className={`${className} bg-[#DCE9F5] flex flex-col rounded-2xl w-lg h-32`} {...props}>
                 <div id="card-header" className="p-3 flex items-start gap-3">
                     <div id="card-icon" className="p-2 rounded-full bg-[#1C385B] text-white">
                         <Icon size={32} />  
