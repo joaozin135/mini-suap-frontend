488b17e882c23a579b714226a00fb6861dd2f919
João Junior <junior.joao1305@gmail.com>
2025-11-18 09:10:23 -0300

    init called screend

--- src/pages/called/index.tsx
+++ src/pages/called/index.tsx
@@ -3,9 +3,16 @@ import { SideBar } from "../../../assets/components/SideBar"
 export function Called(){
     return(
         <main className="flex items-start size-full bg-[#F6F9FC]">
-            <SideBar 
-                text="Mini Suap"
-            />
+            <SideBar text="Mini Suap"/>
+            <aside className="size-full p-4">
+                <nav className="flex justify-end items-center">
+                    <div id="button-new-called" className="flex flex-col rounded-2xl w-fit">
+                        <button>+ Novo chamado</button>
+                    </div>
+                </nav>
+                
+                   
+            </aside>
         </main>
     )
 }
