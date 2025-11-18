05ee432dd56ad3a88b887e73da1648de2429c9a6
João Junior <junior.joao1305@gmail.com>
2025-11-17 21:47:16 -0300

    add component table screen called

---
+++ src/pages/called/components/TableCalled/index.tsx
@@ -0,0 +1,18 @@
+
+interface {
+    text: string
+}
+
+
+export function TableCalled(){
+    return(
+        <table className="bg-white rounded-xl size-full overflow-auto">
+            <tr className="w-full flex">
+                <td className="p-4 max-w-24 flex shrink-0 w-full ">Id</td>
+                <td className="p-4 max-w-md flex shrink-0 w-full">Titulo</td>
+                <td className="p-4 max-w-48 flex shrink-0 w-full">Status</td>
+                <td className="p-4 max-w-48 flex shrink-0 w-full">Ação</td>
+            </tr>   
+        </table>
+    )
+}
