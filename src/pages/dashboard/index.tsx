c7d3e454589b4a5a605847672186ea7db34b784c
João Junior <junior.joao1305@gmail.com>
2025-11-27 19:05:17 -0300

    add component navbar

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,22 +2,14 @@ import { LucideCircleAlert, LucideUserRound } from "lucide-react";
 import { SideBar } from "../../../assets/components/SideBar";
 import { CardTop } from "../../../assets/components/card";
 import { TableTop } from "./components/TableTop";
+import { Navbar } from "../../../assets/components/Navbar";
 
 export function Dashboard (){
     return(
         <main className="flex items-start size-full bg-[#F6F9FC]">
             <SideBar text="Mini Suap"/>
             <aside className="size-full p-4">
-                <nav className="flex justify-end items-center">
-                    <div id="card" className=" flex flex-col rounded-2xl w-fit">
-                        <div id="card-header" className="p-3 flex items-center gap-3">
-                            <div id="card-icon" className="p-2 rounded-full bg-[#D9D9D9]">
-                                <LucideUserRound size={22} />  
-                            </div>
-                            <h1 className="text-lg font-medium">user</h1>
-                        </div>
-                    </div>
-                </nav>
+                <Navbar />
                 <section className="space-y-12">
                     <article className="space-y-8">
                         <h1 className="font-bold text-3xl">Tela inicial</h1>
