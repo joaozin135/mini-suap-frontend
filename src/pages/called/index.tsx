2c366847855886c5c42c30185108ee0f3aad4c3c
João Junior <junior.joao1305@gmail.com>
2025-11-27 19:03:32 -0300

    add component navbar

--- src/pages/called/index.tsx
+++ src/pages/called/index.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "../../../assets/components/Navbar"
 import { SideBar } from "../../../assets/components/SideBar"
 
 export function Called(){
@@ -5,11 +6,7 @@ export function Called(){
         <main className="flex items-start size-full bg-[#F6F9FC]">
             <SideBar text="Mini Suap"/>
             <aside className="size-full p-4">
-                <nav className="flex justify-end items-center">
-                    <div id="button-new-called" className="flex flex-col rounded-2xl w-fit">
-                        <button>+ Novo chamado</button>
-                    </div>
-                </nav>
+                <Navbar />
                 
                    
             </aside>
