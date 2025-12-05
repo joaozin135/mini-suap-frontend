a3b0f4347e35816d61ba50e0e5950bc066615458
João Junior <junior.joao1305@gmail.com>
2025-12-05 16:27:28 -0300

    add components

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,26 +8,45 @@ export function Dashboard (){
     return(
         <main className="flex items-start size-full bg-[#F6F9FC]">
             <SideBar text="Mini Suap"/>
-            <aside className="size-full">
+            <aside className="size-full flex flex-col">
                 <Navbar />
-                <section className="space-y-12 p-4">
-                    <article className="space-y-8 p-4">
+                <section className="p-4">
+                    <div>    
                         <h1 className="font-bold text-3xl">Tela inicial</h1>
-                        <div id="card-row" className="flex justify-start gap-8 items-start">
-                        <CardTop 
+                        <h2 className="text-[#797979]">Bem vindo ao Mini Suap!</h2>
+                    </div>
+                    <div className="space-y-2 p-4 flex flex-col">
+                        <div id="card-row" className="grid auto-rows-min gap-4 md:grid-cols-4 items-start space-y-4">
+                        <CardTop
+                            className="bg-white" 
                             icon={LucideCircleAlert}
-                            text="Chamados Abertos"
+                            text="Total de Chamados"
                             value="17"
+                            description="+12% comparado ao último mês"
                         />
-
                         <CardTop 
                             className="bg-white"
                             icon={LucideUserRound}
-                            text="Usuários"
+                            text="Chamados Abertos"
                             value="29"
+                            description="-7% comparado ao último mês"
+                        />
+                        <CardTop 
+                            className="bg-white"
+                            icon={LucideUserRound}
+                            text="Resolvidos Hoje"
+                            value="29"
+                            description="+8% comparado ao último mês"
+                        />
+                        <CardTop 
+                            className="bg-white"
+                            icon={LucideUserRound}
+                            text="Chamados em andamento"
+                            value="29"
+                            description="+5% comparado ao última semana"
                         />
                         </div>
-                    </article> 
+                    </div> 
                     <article className="space-y-4 items-center">
                         <h1 className="text-2xl font-semibold">Chamados recentes</h1>
                         <TableTop />
