b03b7f11ce97dc42605db1e242b2a840c648c919
João Junior <junior.joao1305@gmail.com>
2025-12-05 16:27:14 -0300

    init create scree calleds

--- src/pages/called/index.tsx
+++ src/pages/called/index.tsx
@@ -7,7 +7,13 @@ export function Called(){
             <SideBar text="Mini Suap"/>
             <aside className="size-full">
                 <Navbar />
-                
+                <div className="p-4">
+                    <div>
+                        <h1 className="font-bold text-3xl">Chamados</h1>
+                        <h2 className="text-[#797979]">Chamados abertos recentemente</h2>
+                    </div>
+                    
+                </div>
                    
             </aside>
         </main>
