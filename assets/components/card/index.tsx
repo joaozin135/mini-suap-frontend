bc15f3594f45b2879995f581ad073fc2690f05f2
João Junior <junior.joao1305@gmail.com>
2025-12-05 16:26:45 -0300

    add components

--- assets/components/card/index.tsx
+++ assets/components/card/index.tsx
@@ -5,23 +5,21 @@ import type { DetailedHTMLProps } from "react";
 interface cardProps extends DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
     icon: LucideIcon;
     text: string;
+    description: string;
     value: string;
 }
 
-export function CardTop({ icon: Icon, text, value, className, ...props}: cardProps){
+export function CardTop({ icon: Icon, text, value, description, className, ...props}: cardProps){
     return(
-
-        
-
-        
-            <div id="card" className={`${className} bg-[#DCE9F5] flex flex-col rounded-2xl w-lg h-32`} {...props}>
+            <div id="card" className={`${className} bg-[#DCE9F5] flex flex-col rounded-2xl w-sm h-32 border-1 border-border`} {...props}>
                 <div id="card-header" className="p-3 flex items-start gap-3">
                     <div id="card-icon" className="p-2 rounded-full bg-background text-white">
                         <Icon size={32} />  
                     </div>  
                     <div className="flex flex-col gap-2 ">
-                        <h2 className="text-xl font-medium">{text}</h2>
-                        <h1 className="text-4xl font-bold">{value}</h1>
+                        <h2 className="text-xl font-regular">{text}</h2>
+                        <h1 className="text-4xl font-semibold">{value}</h1>
+                        <h3 className="font-light text-border">{description}</h3>
                     </div>
                 </div>
             </div>
