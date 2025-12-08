30a52594576e4fe0855c5a3cfb221734f1525cd7
João Junior <junior.joao1305@gmail.com>
2025-12-08 01:20:30 -0300

    start of creation page called

--- src/pages/called/index.tsx
+++ src/pages/called/index.tsx
@@ -1,3 +1,4 @@
+import { LucideCircleCheck, LucideSquarePen, LucideTrash } from "lucide-react"
 import { Navbar } from "../../../assets/components/Navbar"
 import { SideBar } from "../../../assets/components/SideBar"
 
@@ -8,12 +9,27 @@ export function Called(){
             <aside className="size-full">
                 <Navbar />
                 <div className="p-4">
-                    <div>
+                    <div className="">
                         <h1 className="font-bold text-3xl">Chamados</h1>
                         <h2 className="text-[#797979]">Chamados abertos recentemente</h2>
                     </div>
-                    
+
+                    <div className="bg-white w-full flex flex-col p-8 space-y-4 rounded-lg border-border border-1">
+                        <div className="text-2xl">1 Chamado</div>
+                        <div className="border-1 border-border p- rounded-lg">
+                            <div className="p-4">
+                                <h2 className="text-border text-lg">HD-001</h2>
+                                <h1 className="font-regular text-xl">Problemas com Dispositivo Móvel</h1>
+                            </div>
+                            <div className="flex flex-row justify-center p-4 gap-10">
+                                <button><LucideSquarePen size={32}/></button>
+                                <button><LucideTrash size={32}/></button>
+                                <button><LucideCircleCheck size={32}/></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
                    
             </aside>
         </main>
